48e816f7c025dc82c2eefd47bd9fa6857802e684
supremozee <supremozee14@gmail.com>
2025-06-03 11:06:02 +0100

    config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ["images.unsplash.com"]
+  },
+  env: {
+    RESEND_API_KEY2: process.env.RESEND_API_KEY2,
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "1mb",
+      allowedOrigins: ["*"],
+    },
   }
 };
 
